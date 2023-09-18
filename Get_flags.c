ddfd9e923ad77a6a578d323c8934d2f79160374a
Ruba124 <r.m.d.abkam@gmail.com>
2023-09-18 16:54:19 -0700

    fixed

--- Get_flags.c
+++ Get_flags.c
@@ -15,8 +15,12 @@ int g_flags(const char *format, int *i)
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[6];
 
-	FLAGS_ARR[6] = {FUN_MINUS, FUN_PLUS, FUN_ZERO, FUN_HASH, FUN_SPACE, 0};
-
+	FLAGS_ARR[6] = FUN_MINUS;
+   	FLAGS_ARR[7] = FUN_PLUS;
+   	FLAGS_ARR[8] = FUN_ZERO;
+   	FLAGS_ARR[9] = FUN_HASH;
+   	FLAGS_ARR[10] = FUN_SPACE;
+   	FLAGS_ARR[11] = 0;
 	for (c_i = *i + 1; format[c_i] != '\0'; c_i++)
 	{
 		for (r = 0; FLAGS_CH[r] != '\0'; r++)
